fbba4a22afbc3888abbe0ec73e63c889f1c1d1c1
guzlej <martin.guzlej@gmail.com>
2020-12-22 18:50:24 +0000

    deleted a tag without href "View profile" page

--- src/index.js
+++ src/index.js
@@ -13,7 +13,6 @@ const lastName = faker.name.lastName()
 const App = () => {
   return (      
       <div className="flex items-center h-screen w-full justify-around">
-        
         <div className="max-w-xs">
           <div className="bg-white shadow-xl rounded-lg py-3">
             <div className="photo-wrapper p-2">
@@ -30,22 +29,21 @@ const App = () => {
                     <p>{ faker.name.jobTitle()}</p>
                 </div>
                 <table className="text-xs my-3">
-                    <tbody><tr>
-                        <td className="px-2 py-2 text-gray-500 font-semibold">Address</td>
-                        <td className="px-2 py-2">{ `${faker.address.country()}, ${faker.address.city()} ` }</td>
+                  <tbody>
+                    <tr>
+                      <td className="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                      <td className="px-2 py-2">{ `${faker.address.country()}, ${faker.address.city()} ` }</td>
                     </tr>
                     <tr>
-                        <td className="px-2 py-2 text-gray-500 font-semibold">Phone</td>
-                        <td className="px-2 py-2">{ `+44/${faker.phone.phoneNumberFormat()}` }</td>
+                      <td className="px-2 py-2 text-gray-500 font-semibold">Phone</td>
+                      <td className="px-2 py-2">{ `+44/${faker.phone.phoneNumberFormat()}` }</td>
                     </tr>
                     <tr>
-                        <td className="px-2 py-2 text-gray-500 font-semibold">Email</td>
-                        <td className="px-2 py-2">{ `${firstName}.${lastName}@${faker.company.companyName()}.com` }</td>
+                      <td className="px-2 py-2 text-gray-500 font-semibold">Email</td>
+                      <td className="px-2 py-2">{ `${firstName}.${lastName}@${faker.company.companyName()}.com` }</td>
                     </tr>
-                </tbody></table>
-                <div className="text-center my-3">
-                    <a className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
-                </div>
+                  </tbody>
+                </table>
             </div>
           </div>
         </div>
