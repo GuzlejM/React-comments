6c17c4fd494199543545f27c3dd9e6dfd06a5613
guzlej <martin.guzlej@gmail.com>
2020-12-22 18:14:14 +0000

    faker imported, set details of person

--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-const App = () => {
-    const buttonText = { text: 'Click Me!' };
+import faker from 'faker';
 
-    return (
-        <div>
-            Hello world !
+
+// TODO 
+    // Refactor separate to components,
+    // Import faker databases
+        
+const App = () => {
+  return (
+    <div>
+      <div class="flex items-center h-screen w-full justify-center">
+        <div class="max-w-xs">
+        <div class="bg-white shadow-xl rounded-lg py-3">
+        <div class="photo-wrapper p-2">
+            <img class="w-32 h-32 rounded-full mx-auto" src={ faker.image.people() } alt="John Doe" />
         </div>
-    );
+        <div class="p-2">
+            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">Joh Doe</h3>
+            <div class="text-center text-gray-400 text-xs font-semibold">
+                <p>Web Developer</p>
+            </div>
+            <table class="text-xs my-3">
+                <tbody><tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                    <td class="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
+                    <td class="px-2 py-2">+977 9955221114</td>
+                </tr>
+                <tr>
+                    <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
+                    <td class="px-2 py-2">john@exmaple.com</td>
+                </tr>
+            </tbody></table>
+
+            <div class="text-center my-3">
+                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
+            </div>
+
+        </div>
+    </div></div>
+        </div>
+      </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
