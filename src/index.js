16a3fa01e55605fbcbb550d8b2a15e541540d13f
guzlej <martin.guzlej@gmail.com>
2020-12-22 18:44:11 +0000

    card working with faker

--- src/index.js
+++ src/index.js
@@ -6,44 +6,50 @@ import faker from 'faker';
 // TODO 
     // Refactor separate to components,
     // Import faker databases
+
+const firstName = faker.name.firstName()
+const lastName = faker.name.lastName()
         
 const App = () => {
-  return (
-    <div>
-      <div class="flex items-center h-screen w-full justify-center">
-        <div class="max-w-xs">
-        <div class="bg-white shadow-xl rounded-lg py-3">
-        <div class="photo-wrapper p-2">
-            <img class="w-32 h-32 rounded-full mx-auto" src={ faker.image.people() } alt="John Doe" />
-        </div>
-        <div class="p-2">
-            <h3 class="text-center text-xl text-gray-900 font-medium leading-8">Joh Doe</h3>
-            <div class="text-center text-gray-400 text-xs font-semibold">
-                <p>Web Developer</p>
+  return (      
+      <div className="flex items-center h-screen w-full justify-around">
+        
+        <div className="max-w-xs">
+          <div className="bg-white shadow-xl rounded-lg py-3">
+            <div className="photo-wrapper p-2">
+                <img 
+                  className="w-32 h-32 rounded-full mx-auto" 
+                  src={ faker.image.business() } 
+                  alt={ `${firstName}, ${lastName} ` } />
             </div>
-            <table class="text-xs my-3">
-                <tbody><tr>
-                    <td class="px-2 py-2 text-gray-500 font-semibold">Address</td>
-                    <td class="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
-                </tr>
-                <tr>
-                    <td class="px-2 py-2 text-gray-500 font-semibold">Phone</td>
-                    <td class="px-2 py-2">+977 9955221114</td>
-                </tr>
-                <tr>
-                    <td class="px-2 py-2 text-gray-500 font-semibold">Email</td>
-                    <td class="px-2 py-2">john@exmaple.com</td>
-                </tr>
-            </tbody></table>
-
-            <div class="text-center my-3">
-                <a class="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
+            <div className="p-2">
+                <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
+                    { `${firstName}, ${lastName} ` }
+                </h3>
+                <div className="text-center text-gray-400 text-xs font-semibold">
+                    <p>{ faker.name.jobTitle()}</p>
+                </div>
+                <table className="text-xs my-3">
+                    <tbody><tr>
+                        <td className="px-2 py-2 text-gray-500 font-semibold">Address</td>
+                        <td className="px-2 py-2">{ `${faker.address.country()}, ${faker.address.city()} ` }</td>
+                    </tr>
+                    <tr>
+                        <td className="px-2 py-2 text-gray-500 font-semibold">Phone</td>
+                        <td className="px-2 py-2">{ `+44/${faker.phone.phoneNumberFormat()}` }</td>
+                    </tr>
+                    <tr>
+                        <td className="px-2 py-2 text-gray-500 font-semibold">Email</td>
+                        <td className="px-2 py-2">{ `${firstName}.${lastName}@${faker.company.companyName()}.com` }</td>
+                    </tr>
+                </tbody></table>
+                <div className="text-center my-3">
+                    <a className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium" href="#">View Profile</a>
+                </div>
             </div>
-
+          </div>
         </div>
-    </div></div>
-        </div>
-      </div>
+    </div>
   );
 };
 
