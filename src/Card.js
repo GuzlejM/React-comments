82d9a02cdf23b5c6137c97faed5a28d3030708b4
guzlej <martin.guzlej@gmail.com>
2020-12-23 21:54:41 +0000

    fixing same picture bug

--- src/Card.js
+++ src/Card.js
@@ -2,23 +2,17 @@ import React from 'react';
 import faker from 'faker';
 import CardDetail from './CardDetail';
 import ApprovalCard from './ApprovalCard';
-import RoundedProfileImg from './RoundedProfileImg';
 
 console.log(faker)
 
 const Card = () => {
     return (
-        <div className="mt-5 w-64 h-96">
-          <div className="bg-white shadow-xl rounded-lg py-3">
-            <RoundedProfileImg 
-            avatar={faker.image.unsplash.people()}
-            />
+        <div className="mt-20 mx-5 lg:mt-0 w-64 h-96">
             <CardDetail
               author={`${faker.name.firstName()} ${faker.name.lastName()}`}
             />
             <ApprovalCard />
           </div>
-        </div>
     );
 };
 
