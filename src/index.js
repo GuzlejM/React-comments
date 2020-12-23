a7c3f4da61ad56eeca1f72210098cfcb772dadfa
guzlej <martin.guzlej@gmail.com>
2020-12-23 17:23:41 +0000

    faker is meaningfull now, images different, tailwind working mobile and desktop

--- src/index.js
+++ src/index.js
@@ -3,30 +3,26 @@ import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
-
 // TODO 
     // Refactor separate to components,
     // Import faker databases
-
-const firstName1 = faker.name.firstName()
-const firstName2 = faker.name.firstName()
-const firstName3 = faker.name.firstName()
         
 const App = () => {
   return (      
-      <div className="flex flex-col md:flex-row items-center h-screen w-full justify-around">
+      <div className="flex sm:flex-nowrap md:flex-wrap flex-col md:flex-row items-center md:h-screen w-full justify-around">
         <CommentDetail 
-          authorFirstName={firstName1}
-          avatar={faker.image.people()}
-        
+          author={`${faker.name.firstName()} ${faker.name.lastName()}`}
+          avatar={ faker.image.people() }
         />
         <CommentDetail 
-          authorFirstName={firstName2}
-          avatar={faker.image.people()}
+          author={`${faker.name.firstName()} ${faker.name.lastName()}`}
+          avatar={ faker.image.animals() }
+
         />
         <CommentDetail 
-          authorFirstName={firstName3}
-          avatar={faker.image.people()}
+          author={`${faker.name.firstName()} ${faker.name.lastName()}`}
+          avatar={ faker.image.abstract() }
+
         />
     </div>
   );
